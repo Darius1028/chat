1f804844c75cc09a74833c3a65120085000f2e41
Dario Morakes <dariodbml@gmail.com>
2023-02-08 09:31:57 -0500

    add asset

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -2,7 +2,7 @@ import flask
 from flask import Flask
 from chatbotconfig import Config
 
-app=Flask(__name__, static_url_path='/asset')
+app=Flask(__name__, static_url_path='', static_folder='asset', template_folder='templates')
 
 app.config.from_object(Config)
 
